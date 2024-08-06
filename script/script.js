36bd4e23e2fcba9d4ea5296084834cb698c371a3
MrCivic7 <161588529+MrCivic7@users.noreply.github.com>
2024-08-06 08:20:30 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let vetor = [];
+
+function adiconarValor ();
+    const valor=Number(document.getElementById)('tabela valores'). getElementByTagName('tbody')[0];
+    cons novalinha=tabela.insertROW();
+    const celula=novalinha.insertCell(0);
+    celula.textContent=valor;
+    document.getElementById('valor').value= ;
+    document.getElementById('valor').focus();
